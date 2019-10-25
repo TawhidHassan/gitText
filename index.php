f97c4064d26c1e4acf910e77535edd343c802f1b
Tawhid Hassan <43006217+TawhidHassan@users.noreply.github.com>
2019-10-25 18:57:18 +0600

    done merge

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
 echo sifat;
-<<<<<<< HEAD
+
 var s;
 function s()
 {
     s+sifat;
 }
 
-=======
-
 function king()
 {
     callme();
 }
 ?>
+
 <?php
 function callme()
 {
  echo "hello sifat";
 }
->>>>>>> branch5
+
 ?>
 
